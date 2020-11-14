62d1d897edcaf13376596f2f778039fc089acf3d
konrad-sztenderski <sztenk07@gmail.com>
2020-11-14 18:27:43 +0100

    Add vuex

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,13 @@
 import {createApp} from 'vue'
+// @ts-ignore
+import {createStore} from 'vuex'
 import App from '@/App.vue';
 import router from '@/router';
 
-const app = createApp(App).use(router);
+console.log(createStore)
+
+const app = createApp(App)
+    .use(router)
+    .use(createStore({}));
 
 app.mount('#app');
