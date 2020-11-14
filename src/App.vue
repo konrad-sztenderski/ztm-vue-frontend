20c8decaf2f147139f85a9c2fa5c197d928865f3
konrad-sztenderski <sztenk07@gmail.com>
2020-11-14 17:24:43 +0100

    Change import paths

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
 
 <script lang="ts">
 import {Options, Vue} from 'vue-class-component';
-import HelloWorld from './components/HelloWorld.vue';
+import HelloWorld from '@/components/HelloWorld.vue';
 
 @Options({
   components: {
