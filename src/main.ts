65a1e74850dac156b81fd2c5a8d05ce66ea7187b
konrad-sztenderski <sztenk07@gmail.com>
2020-11-15 15:16:59 +0100

    Squashed commit of the following:     Set production path     Add bootstrap     Add vuex     Add vue-router     Change import paths

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,16 @@
-import {createApp} from 'vue'
-import App from './App.vue'
+import {createApp} from 'vue';
+// @ts-ignore
+import {createStore} from 'vuex';
 
-createApp(App).mount('#app')
+import 'jquery'
+import 'bootstrap/dist/js/bootstrap.bundle.js';
+import 'bootstrap/dist/css/bootstrap.css'
+
+import App from '@/App.vue';
+import router from '@/router';
+
+const app = createApp(App)
+    .use(router)
+    .use(createStore({}));
+
+app.mount('#app');
